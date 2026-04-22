590cc704d03d2890dabe4f61749b30a634b29691
Aline Valente <alinevalente425@gmail.com>
2026-04-22 10:03:14 -0400

    Versão final: Interface com CustomTkinter
    - Implementação da interface moderna com CustomTkinter.
    - Refatoração da lógica de cálculo para suporte a float.
    - Adição de tratamento para exibir aprovação/reprovação visualmente.

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ app = ctk.CTk()
 app.title('Calculadora de media')
 #definindo tamanho da tela
 app.geometry('300x300')
-#O programa entra em estado de espera, reagindo aos cliques e movimentos do mouse.
 #criando campos
-#label - caixa de texto
-#campo_nota1 = ctk.CTkLabel(app, text='Nota 1?')
-#Espaço
-#label_nota1.pack(pady=10)
+label = ctk.CTkLabel(app, text="")
+label1 = ctk.CTkLabel(app, text="")
+
 def media_nota():
   try:
 
@@ -21,19 +19,20 @@ def media_nota():
     N3 = float(caixa_nota3.get().replace(',', '.'))
     media = (N1+N2+N3)/3
     if media>=6:
-        label = ctk.CTkLabel(app, text='Aprovado')
-        label.pack(pady=10)
-        label1 = ctk.CTkLabel(app, text= media)
-        label1.pack()
+     label1.configure(text=f"Média: {media:.2f}")   
+     label1.pack(pady=10)
+     label.configure(text="Aprovado")
+     label.pack()
     else:
-        label2 = ctk.CTkLabel(app, text='Reprovado')
-        label2.pack(pady=10)
-        label3 = ctk.CTkLabel(app, text= media)
-        label3.pack()
+      label1.configure(text=f"Média: {media: .2f}")
+      label1.pack(pady=10)
+      label.configure(text="Reprovado")
+      label.pack()
   except:
-
-    label9 = ctk.CTkLabel(app, text='Preencha os campos apenas com números!')
-    label9.pack()
+    label.configure(text="Por favor, utilize apenas números nos campos")
+    label.pack()
+    label1.configure(text="")
+    label1.pack()
 
 #entry - campo
 
@@ -43,9 +42,11 @@ caixa_nota2 = ctk.CTkEntry(app, placeholder_text='Digite nota 2:')
 caixa_nota2.pack(pady=10)
 caixa_nota3 = ctk.CTkEntry(app, placeholder_text='Digite nota 3:')
 caixa_nota3.pack(pady=10)
+#Botão
 botao_calcular = ctk.CTkButton(app, text='Calcular', command=media_nota)
-botao_calcular.pack(pady=10)
+botao_calcular.pack()
 
+#O programa entra em estado de espera, reagindo aos cliques e movimentos do mouse.
 app.mainloop()
 
 
